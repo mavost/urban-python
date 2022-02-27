31f814017e1390d330d9c24ff020705a0aa55daa
Markus von Steht <markus.von.steht@gmx.de>
2022-02-27 17:32:38 +0100

    Linter update

--- Weigend/Exercises/chap04_asgn05.py
+++ Weigend/Exercises/chap04_asgn05.py
@@ -1,11 +1,11 @@
-#Python3,mvs, 2017-09-27
-#Chapter 4 / Assignment 5
+# Python3,mvs, 2017-09-27
+# Chapter 4 / Assignment 5
 from random import shuffle
 
-charts=[]
+charts = []
 while True:
     try:
-        title=str(input("Enter Song Title:"))
+        title = str(input("Enter Song Title:"))
         if not title:
             print("Proceed after data entry stopped.")
             break
@@ -13,13 +13,12 @@ while True:
     except ValueError:
         print("Proceed after bad entry.")
         break
-if not charts :
+if not charts:
     print("Terminated - Missing Data.")
     exit()
 
 shuffle(charts)
 
-print("Charts of this week:") 
-for a,song in enumerate(charts):
+print("Charts of this week:")
+for a, song in enumerate(charts):
     print("Song "+str(a+1)+": " + song)
-
