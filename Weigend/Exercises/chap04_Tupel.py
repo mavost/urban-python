fc588939786b1679704307797ed2e708f54589b9
Markus von Steht <markus.von.steht@gmx.de>
2022-02-27 16:19:24 +0100

    updating system

--- Weigend/Exercises/chap04_Tupel.py
+++ Weigend/Exercises/chap04_Tupel.py
@@ -1,18 +1,18 @@
-#Python3,mvs, 2017-09-27
+# Python3,mvs, 2017-09-27
 
-list1=[]
+list1 = []
 while True:
     try:
-        x=float(input("X-coordinate bitte:"))
-        y=float(input("Y-coordinate bitte:"))
-        z=float(input("Z-coordinate bitte:"))
+        x = float(input("X-coordinate bitte:") or 0)
+        y = float(input("Y-coordinate bitte:") or 0)
+        z = float(input("Z-coordinate bitte:") or 0)
     except ValueError:
         print("Eingabe beendet.")
         break
-    koordinate=(x,y,z)
+    koordinate = (x, y, z)
     list1.append(koordinate)
     print(str(koordinate) + " was saved.")
 print("Final List:")
 print(list1)
-#Third Value(i.e., z) in second data set
-#print(list1[1][2])
+# Third Value(i.e., z) in second data set
+# print(list1[1][2])
